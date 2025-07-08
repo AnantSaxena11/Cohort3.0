1977107f0e7c8d860b6fe88ea1b84fe42f70a218
Anant Saxena <anantsaxena5454@gmail.com>
2025-07-08 23:56:11 +0530

    Made the layout for the website and somewhat understood tailwind css

--- Web Dev/Tailwind trying Prabhleen/src/App.jsx
+++ Web Dev/Tailwind trying Prabhleen/src/App.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react"
+
 function App() {
-  function onchange()
-  {
-    document.querySelector("html").classList.toggle("dark",document.querySelector("html").classList[0] == "dark" ? false : true)
-  }
+
+  const [sideBar, setSidebar] = useState(true)
   return (
-    <div className= "h-screen bg-white dark:bg-blue-800">
-      <button onClick={onchange}>Mode Change</button>
+    <div className="flex">
+      <SideBar />
+      <MainContent />
     </div>
   )
 }
 
+function SideBar() {
+  return (
+    <div className="bg-green-800 w-96 h-screen">
+      I am a SideBar
+    </div>
+  )
+}
+function MainContent() {
+  return (
+    <div className="w-full">
+      <div className="h-48 bg-black">
+      </div>
+      <div className="grid grid-cols-10 gap-8">
+        <div className="col-span-10 md:col-span-2 h-96 rounded-2xl shadow bg-red-200 -translate-y-12"></div>
+        <div className="col-span-10 md:col-span-5 h-96 rounded-2xl shadow bg-orange-500 c"></div>
+        <div className="col-span-10 md:col-span-3 h-96 rounded-2xl shadow bg-purple-500 "></div>
+      </div>
+    </div>
+  )
+}
 export default App
